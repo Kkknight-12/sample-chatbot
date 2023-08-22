01545a832b61eb686cbc9c42bf0b343477cebc1b
Kkknight-12 <mayanks365@gmail.com>
2023-08-22 18:05:02 +0530

    Ui fixes

--- components/samplebot/index.tsx
+++ components/samplebot/index.tsx
@@ -6,61 +6,49 @@ import { useChat } from 'ai/react';
 const Chatbot = () => {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
 
-  // const [messages, setMessages] = useState<any>([]);
-  // const [input, setInput] = useState<any>('');
-
-  // const handleInputChange = (e: any) => {
-  //   setInput(e.target.value);
-  // };
-
-  // const handleSubmit = (e: any) => {
-  //   e.preventDefault();
-
-  //   setMessages([...messages, { text: input, user: true }]);
-
-  //   const chatbotResponse =
-  //     "Hello! I'm your chatbot. How can I assist you today?";
-
-  //   setMessages([...messages, { text: chatbotResponse, user: false }]);
-
-  //   setInput('');
-  // };
-
   return (
-    <div className='flex flex-col items-center justify-center h-screen gap-4'>
-      <div className='flex-grow bg-gray-100 p-4 overflow-auto w-1/3 rounded'>
-        <div className='flex flex-col-reverse gap-4'>
-          {messages.map((message, index) => (
-            <div
-              key={index}
-              className={`rounded-lg p-2 ${
-                message.role
-                  ? 'bg-blue-300 text-white self-end'
-                  : 'bg-gray-300 text-black self-start'
-              }`}
+    <div className='container mx-auto max-w-[700px]'>
+      <div className='flex flex-col h-screen bg-gray-900'>
+        <div className='flex-grow p-6'>
+          <div className='flex flex-col space-y-4 '>
+            {messages.map((message, index) => (
+              <div
+                key={index}
+                className={`flex p-2 ${
+                  message.role === 'user' ? ' justify-end' : ' justify-start'
+                }`}
+              >
+                <div
+                  className={`${
+                    message.role === 'user'
+                      ? 'bg-purple-500 clip-right ps-1 rounded-s-md'
+                      : 'bg-gray-800 clip-left pe-1 rounded-e-md'
+                  }   p-5 text-white max-w-sm`}
+                >
+                  {message.content}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <form className='flex-none p-6' onSubmit={handleSubmit}>
+          <div className='flex rounded-lg border border-gray-700 bg-gray-800'>
+            <input
+              type='text'
+              value={input}
+              onChange={handleInputChange}
+              className='flex-grow px-4 py-2 bg-transparent text-white focus:outline-none'
+              placeholder='Type your message...'
+            />
+            <button
+              type='submit'
+              className='bg-purple-500 rounded-lg px-4 py-2 text-white font-semibold focus:outline-none hover:bg-purple-600 transition-colors duration-300'
             >
-              {message.content}
-            </div>
-          ))}
-        </div>
+              Send
+            </button>
+          </div>
+        </form>
       </div>
-      <form className='bg-white p-4 w-1/3 rounded' onSubmit={handleSubmit}>
-        <div className='flex space-x-2'>
-          <input
-            type='text'
-            value={input}
-            onChange={handleInputChange}
-            className='flex-grow border rounded-lg p-2'
-            placeholder='Type your message...'
-          />
-          <button
-            type='submit'
-            className='bg-blue-500 text-white rounded-lg p-2'
-          >
-            Send
-          </button>
-        </div>
-      </form>
     </div>
   );
 };
